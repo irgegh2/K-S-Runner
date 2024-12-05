97c9323b54afb46e8b970aa1786a8a5635b1e73f
Irgegh <User@K20924>
2024-12-05 10:41:00 +0300

    Прыжок и двойной прыжок

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float playerSpeed;
-    public float jumpForce;
-    private float fallMulti = 7f;
-    private float jumpMulti = 3f;
-    private int jumpCount;
-    private bool canJump = true;
+    Animator anim;
+
+    public float player_speed;
+    public float jump_force;
+    public LayerMask layer;
+    public Transform transfotm;
+
+    private float fall_multi = 11f;
+    private float deb = 0.1f;
+    private int jump_count = 0;
+    private bool can_jump = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = Vector3.right * playerSpeed * Time.fixedDeltaTime + transform.position;
+        Collider2D isGrounded = Physics2D.OverlapCircle(transfotm.position, 0.05f, layer);
+        transform.position = Vector3.right * player_speed * Time.fixedDeltaTime + transform.position;
+        deb += Time.fixedDeltaTime;
 
-        if (Input.GetMouseButton(0))
+        if (isGrounded)
         {
-            rb.velocity = Vector3.up * jumpForce;
+            can_jump = true;
+            jump_count = 0;
         }
+
+        if (deb > 0.1f && can_jump == true && (isGrounded || jump_count == 1))
+        {
+            if ((Input.touchCount > 0 || Input.GetMouseButton(0)))
+            {
+                anim.SetTrigger("jump");
+                deb = 0;
+                jump_count += 1;
+                rb.velocity = Vector3.up * jump_force;
+            }
+        }
+
+        if (jump_count == 2)
+            can_jump = false;
+
         if (rb.velocity.y < 0)
-        {
-            rb.AddForce(Vector3.down * fallMulti);
-        } else if (rb.velocity.y > 0)
-        {
-            rb.AddForce(Vector3.up * jumpMulti);
-        }
+            rb.AddForce(Vector3.down * fall_multi);
     }
 }
